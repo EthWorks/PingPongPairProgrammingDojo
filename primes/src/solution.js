743726ffb6ce1dffe8f669a4f78d91350dbb42ca
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-11-24 16:53:51 +0100

    Add primes solution

--- primes/src/solution.js
+++ primes/src/solution.js
@@ -1,3 +1,20 @@
-exports.sumPrimes = function () {
+exports.sumPrimes = (anArray) => 
+  anArray
+    .filter(isPrime)
+    .reduce((acc, el) => acc + el, 0);
 
+
+function isPrime(x) {
+  if (typeof x !== "number") {
+    throw new TypeError('Invalid input');
+  }
+  if (x === 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= x; i++) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
--- primes/src/test.js
+++ primes/src/test.js
@@ -1,7 +1,54 @@
 const { sumPrimes } = require('./solution');
 
 it('works', () => {
+  expect(sumPrimes([3])).toBe(3);
 });
+
+it('works for non prime', () => {
+  expect(sumPrimes([4])).toBe(0);
+});
+
+it('works for another non prime', () => {
+  expect(sumPrimes([6])).toBe(0);
+});
+
+it('works non prime', () => {
+  expect(sumPrimes([5])).toBe(5);
+});
+
+it('works for non prime', () => {
+  expect(sumPrimes([4])).toBe(0);
+});
+
+it('works for prime', () => {
+  expect(sumPrimes([13])).toBe(13);
+});
+
+it('works for multiple', () => {
+  expect(sumPrimes([2, 13])).toBe(15);
+});
+
+it('works for multiple', () => {
+  expect(sumPrimes([9, 13])).toBe(13);
+});
+
+it('works for 1', () => {
+  expect(sumPrimes([1])).toBe(0);
+});
+
+it('works for empty', () => {
+  expect(sumPrimes([])).toBe(0);
+});
+
+it('to throw', () => {
+  expect(() => sumPrimes([undefined])).toThrow(TypeError);
+});
+
+it('works for 25', () => {
+  expect(sumPrimes([25])).toBe(0);
+});
+
+
 
 // check ===
 // expect(1).toBe(1)
