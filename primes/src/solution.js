795b715e65b2f67f5238bd6a9e977d5b5edb9e4c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-12-09 15:56:49 +0100

    Group 3 solutions

--- primes/src/solution.js
+++ primes/src/solution.js
@@ -1,3 +1,18 @@
-exports.sumPrimes = function () {
-
+exports.sumPrimes = function (array) {
+  return array
+    .filter(isPrime)
+    .reduce((acc, val) => acc + val, 0)
 }
+
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0)
+      return false;
+  }
+  return true;
+}
+
+exports.isPrime = isPrime
--- primes/src/test.js
+++ primes/src/test.js
@@ -1,8 +1,54 @@
-const { sumPrimes } = require('./solution');
+const { sumPrimes, isPrime } = require('./solution');
 
 it('works', () => {
+  expect(sumPrimes).toBeTruthy();
+  
 });
 
+it('works', () => {
+  expect(sumPrimes([])).toBe(0);
+});
+
+
+it('works', () => {
+  expect(sumPrimes([1])).toBe(0);
+});
+
+it('works', () => {
+  expect(sumPrimes([2])).toBe(2);
+});
+
+it('works', () => {
+expect(sumPrimes([4])).toBe(0);
+});
+
+it('works', () => {
+  expect(sumPrimes([1,2])).toBe(2);
+});
+
+it('works', () => {
+  expect(isPrime(1)).toBe(false);
+});
+
+it('works', () => {
+  expect(isPrime(2)).toBe(true);
+});
+
+it('works', () => {
+  expect(isPrime(4)).toBe(false);
+});
+
+it('works', () => {
+  expect(isPrime(20)).toBe(false);
+});
+
+it('works', () => {
+  expect(isPrime(7)).toBe(true);
+});
+
+it('works', () => {
+  expect(sumPrimes([2,7,20,11,13])).toBe(33);
+});
 // check ===
 // expect(1).toBe(1)
 
