c36ebc65e9c51dc786f1eabeb31b9aeb8725bf8c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-02-21 14:37:17 +0100

    add base for game of life

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,15 @@
+/*
+
+http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
+RULES:
+
+1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+3. Any live cell with two or three live neighbours lives on to the next generation.
+4. Any dead cell with exactly three live neighbours becomes a live cell.
+*/
+
 exports.solve = function () {
 
 }
