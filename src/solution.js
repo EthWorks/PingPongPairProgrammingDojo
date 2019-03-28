58336429a814fb4e03c20be0fe2b1a23ab33eb83
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-03-28 15:59:31 +0100

    Diamond

--- src/solution.js
+++ src/solution.js
@@ -1,13 +1,14 @@
 /*
 
-http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+Given a letter, print a diamond starting with ‘A’ with the supplied letter at the widest point.
 
-RULES:
+For example: print-diamond ‘C’ prints
 
-1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-2. Any live cell with more than three live neighbours dies, as if by overcrowding.
-3. Any live cell with two or three live neighbours lives on to the next generation.
-4. Any dead cell with exactly three live neighbours becomes a live cell.
+  A
+ B B
+C   C
+ B B
+  A
 */
 
 exports.solve = function () {
