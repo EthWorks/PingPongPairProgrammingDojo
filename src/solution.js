856ffbb08666584800d62ce44c3cab4717045e7e
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-05-27 16:36:59 +0200

    Add es6 and typescript support

--- src/solution.js
+++ src/solution.js
@@ -1,16 +1,3 @@
-/*
-
-Given a letter, print a diamond starting with ‘A’ with the supplied letter at the widest point.
-
-For example: print-diamond ‘C’ prints
-
-  A
- B B
-C   C
- B B
-  A
-*/
-
-exports.solve = function () {
+export function solve () {
 
 }
--- src/test.js
+++ src/test.js
@@ -1,10 +1,5 @@
-const { solve } = require('./solution');
+import { solve } from './solution'
 
-it('works', () => {
-});
-
-// check ===
-// expect(1).toBe(1)
-
-// check deep equality
-// expect([1, 2]).toEqual([1, 2])
+it('does nothing', () => {
+  expect(solve()).toEqual(undefined)
+})
